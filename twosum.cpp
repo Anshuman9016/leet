87ab18e67c14a240c315a0ed5550ac438826d3d8
Anshuman9016 <Anshuman.119016@stu.upes.ac.in>
2026-07-13 14:48:23 +0530

    Add files via upload

---
+++ twosum.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int, int> mpp;
+        for (int i = 0; i < nums.size(); i++) {
+            int a = nums[i];
+            int n = target - a;
+            
+            if (mpp.find(n) != mpp.end()) {
+                return {mpp[n], i};
+            }
+            mpp[a] = i;
+        }
+        return {};
+    }
+};
