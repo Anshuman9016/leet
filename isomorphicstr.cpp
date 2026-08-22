688e5bfce4883d8b095aa60df77e4d54e6f2933d
Anshuman Mohapatra <Anshuman.119016@stu.upes.ac.in>
2026-08-22 19:28:12 +0530

    Add files via upload

---
+++ isomorphicstr.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        if (s.length() != t.length()) return false;
+
+        vector<int> mapS(256, 0);
+        vector<int> mapT(256, 0);
+
+        for (int i = 0; i < s.length(); i++) {
+            if (mapS[(unsigned char)s[i]] != mapT[(unsigned char)t[i]]) {
+                return false;
+            }
+            mapS[(unsigned char)s[i]] = i + 1;
+            mapT[(unsigned char)t[i]] = i + 1;
+        }
+
+        return true;
+    }
+};
