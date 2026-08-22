e5af6dd4f72aa6feab0062380a2f7ee497494a0e
Anshuman Mohapatra <Anshuman.119016@stu.upes.ac.in>
2026-08-22 19:27:45 +0530

    Add files via upload

---
+++ strrot2.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        if (s.length() != goal.length()) {
+            return false;
+        }
+
+        string concat =goal+goal;
+        int ind = concat.find(s);
+        if (ind == -1) {
+            return false;
+        }
+        return true;
+        
+    }
+};
